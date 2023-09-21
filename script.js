428d4ffc7fea4efed470c79de5b7b51a5ab65427
o <nesubludai@gmail.com>
2023-09-21 21:41:32 +0300

    css remained

--- script.js
+++ script.js
@@ -1,95 +1,71 @@
-// let firstMove = true;
-
-// const gameBoard = (() => {
-//     const game = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-//     return { game };
-// })();
-
-// const Player = () => {
-//     const move = firstMove ? "X" : 'O';
-//     firstMove = firstMove ? !firstMove : firstMove;
-//     const occupy = function (field) {
-//         if (!field.classList.contains('occupedX') && !field.classList.contains('occupedO')) {
-//             if (move === "X") {
-//                 field.classList.add('occupedX');
-//             } else if (move === "O") {
-//                 field.classList.add('occupedO');
-//             }
-//             field.innerText = move;
-//             firstMove = !firstMove;
-//             console.log(gameBoard.game)
-//         }
-//     }
-//     return { occupy, move }
-// }
-
-// const game = (() => {
-//     const player1 = Player();
-//     const player2 = Player();
-//     return { player1, player2, firstMove }
-// })();
-
-// const displayController = (() => {
-//     const updateScreen = function () {
-//         document.body.innerHTML = '';
-//         gameBoard.game.forEach((move) => {
-//             const moveElement = document.createElement('button');
-//             moveElement.innerText = move;
-//             moveElement.style = 'height: 60px; width: 60px; font-size: 42px;';
-//             moveElement.addEventListener('click', () => {
-//                 const player = firstMove ? game.player1 : game.player2;
-//                 player.occupy(moveElement);
-//                 const index = gameBoard.game.indexOf(move);
-//                 if (!moveElement.classList.contains('occupedX') && !moveElement.classList.contains('occupedO')) {
-//                     gameBoard.game.splice(index, 1, player.move);
-//                 }
-//             });
-//             document.body.appendChild(moveElement);
-//         });
-//     };
-//     return { updateScreen };
-// })();
-
-// displayController.updateScreen();
-
 const gameBoard = (() => {
     let firstMove = true;
+    let canMove = false;
     const fields = ['', '', '', '', '', '', '', '', ''];
+    const board = document.querySelector('#board');
+    let winner = '';
+    let tie = false;
     const winCheck = (move) => {
         // logic :3
-        if ((fields[0] === 'x' && fields[1] === 'x' && fields[2] === 'x') || (fields[0] === 'o' && fields[1] === 'o' && fields[2] === 'o') || (fields[3] === 'x' && fields[4] === 'x' && fields[5] === 'x') || (fields[3] === 'o' && fields[4] === 'o' && fields[5] === 'o') || (fields[6] === 'x' && fields[7] === 'x' && fields[8] === 'x') || (fields[6] === 'o' && fields[7] === 'o' && fields[8] === 'o') || (fields[0] === 'x' && fields[4] === 'x' && fields[8] === 'x') || (fields[0] === 'o' && fields[4] === 'o' && fields[8] === 'o') || (fields[2] === 'x' && fields[4] === 'x' && fields[6] === 'x') || (fields[2] === 'o' && fields[4] === 'o' && fields[6] === 'o') || (fields[0] === 'x' && fields[3] === 'x' && fields[6] === 'x') || (fields[0] === 'o' && fields[3] === 'o' && fields[6] === 'o') || (fields[1] === 'x' && fields[4] === 'x' && fields[7] === 'x') || (fields[1] === 'o' && fields[4] === 'o' && fields[7] === 'o') || (fields[2] === 'x' && fields[5] === 'x' && fields[8] === 'x') || (fields[2] === 'o' && fields[5] === 'o' && fields[8] === 'o')) {
-            console.log(`${move} win`);
-        }   
-    }
-    return {fields, firstMove, winCheck}
+        if ((gameBoard.fields[0] === 'x' && gameBoard.fields[1] === 'x' && gameBoard.fields[2] === 'x') || (gameBoard.fields[0] === 'o' && gameBoard.fields[1] === 'o' && gameBoard.fields[2] === 'o') || (gameBoard.fields[3] === 'x' && gameBoard.fields[4] === 'x' && gameBoard.fields[5] === 'x') || (gameBoard.fields[3] === 'o' && gameBoard.fields[4] === 'o' && gameBoard.fields[5] === 'o') || (gameBoard.fields[6] === 'x' && gameBoard.fields[7] === 'x' && gameBoard.fields[8] === 'x') || (gameBoard.fields[6] === 'o' && gameBoard.fields[7] === 'o' && gameBoard.fields[8] === 'o') || (gameBoard.fields[0] === 'x' && gameBoard.fields[4] === 'x' && gameBoard.fields[8] === 'x') || (gameBoard.fields[0] === 'o' && gameBoard.fields[4] === 'o' && gameBoard.fields[8] === 'o') || (gameBoard.fields[2] === 'x' && gameBoard.fields[4] === 'x' && gameBoard.fields[6] === 'x') || (gameBoard.fields[2] === 'o' && gameBoard.fields[4] === 'o' && gameBoard.fields[6] === 'o') || (gameBoard.fields[0] === 'x' && gameBoard.fields[3] === 'x' && gameBoard.fields[6] === 'x') || (gameBoard.fields[0] === 'o' && gameBoard.fields[3] === 'o' && gameBoard.fields[6] === 'o') || (gameBoard.fields[1] === 'x' && gameBoard.fields[4] === 'x' && gameBoard.fields[7] === 'x') || (fields[1] === 'o' && gameBoard.fields[4] === 'o' && gameBoard.fields[7] === 'o') || (gameBoard.fields[2] === 'x' && gameBoard.fields[5] === 'x' && gameBoard.fields[8] === 'x') || (gameBoard.fields[2] === 'o' && gameBoard.fields[5] === 'o' && gameBoard.fields[8] === 'o')) {
+            gameBoard.winner = `${move} is winner`;
+            gameBoard.canMove = false;
+            console.log(gameBoard.winner);
+        } else if (!gameBoard.fields.includes('') && gameBoard.winner === '') {
+            gameBoard.tie = true;
+            gameBoard.canMove = false;
+            console.log('tie');
+        }
+    };
+    const restart = () => {
+        gameBoard.firstMove = true;
+        gameBoard.fields = ['', '', '', '', '', '', '', '', ''];
+        gameBoard.canMove = true;
+        gameBoard.tie = false;
+        gameBoard.winner = '';
+    };
+
+    return {fields, firstMove, winCheck, board, restart, canMove, tie, winner}
 })();
 
 
 const screenController = (() => {
-    const board = document.querySelector('#board')
+    const startButton = document.querySelector('#start-button');
+    const resultElement = document.querySelector('#result');
     const updateScreen = () => {
-        board.innerHTML = '';
+        gameBoard.board.innerHTML = '';
         gameBoard.fields.forEach((field, index) => {
             const fieldElement = document.createElement('button');
             fieldElement.innerText = field;
             fieldElement.classList.add('fieldElement');
             fieldElement.addEventListener('click', () => {
-                if (!fieldElement.classList.contains('occuped')) {
+                if (gameBoard.canMove && field !== 'x' && field !== 'o') {
                     const move = gameBoard.firstMove ? 'x' : 'o'
 
                     gameBoard.fields.splice(index, 1, move);
                     gameBoard.winCheck(move);
 
                     fieldElement.innerText = move;
-                    fieldElement.classList.add(move);
                     fieldElement.classList.add('occuped');
 
                     gameBoard.firstMove = !gameBoard.firstMove;
+                    updateScreen();
                 }
             });
-            board.appendChild(fieldElement);
+            gameBoard.board.appendChild(fieldElement);
+            startButton.innerHTML = (gameBoard.fields.includes('x') || gameBoard.fields.includes('o') || gameBoard.canMove) ? 'Restart' : 'Start';
         });
+        if (gameBoard.winner || gameBoard.tie) {
+            resultElement.classList.remove('non-displied');
+            resultElement.innerText = gameBoard.winner ? gameBoard.winner : 'Tie';
+        }
     }
+
+    startButton.addEventListener('click', () => {
+        gameBoard.restart();
+        updateScreen();
+    });
+
     return {updateScreen}
 })();
 
